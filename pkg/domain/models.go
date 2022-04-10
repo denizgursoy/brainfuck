29b8f97c8db55ed469edda646c551f833e626acb
Deniz <denizgursoy.443@gmail.com>
2022-04-11 00:27:17 +0300

    [ Deniz ] [  ] write doc for types

--- pkg/domain/models.go
+++ pkg/domain/models.go
@@ -6,6 +6,12 @@ const (
 	data_size = 30000
 )
 
+// Brainfuck is the main struct that holds all the data needed to run operations successfully,
+// Data holds cell values. DataPointer show the current cell location which is being modified.
+// Commands slice stores all the command in order they are added.
+// CommandPointer show the last command executed.
+// loopStack holds all the loops and nested loops which are currently executed.
+// IoOptions stores all readers and writers needed.
 type Brainfuck struct {
 	operations     map[rune]Operation
 	Data           [data_size]byte
@@ -16,17 +22,25 @@ type Brainfuck struct {
 	loopStack      []*Loop
 }
 
+// IoOptions holds the all readers or writers that a brainfuck needs.
+// CommandReader reads new command, InputReader reads user input with ',' command,
+// OutputWriter writes current cell value with '.' command.
 type IoOptions struct {
 	CommandReader io.Reader
 	InputReader   io.Reader
 	OutputWriter  io.Writer
 }
 
+// CustomOperation holds a Character that will be read from CommandReader.
+// Operation is the function that will be executed is the Character is read from CommandReader.
 type CustomOperation struct {
 	Character rune
 	Operation Operation
 }
 
+// Loop holds data of a loop created by the commands.
+// Start stores the  position '[' command on Commands slice.
+// End stores the  position ']' command on Commands slice.
 type Loop struct {
 	Start *int64
 	End   *int64
--- pkg/domain/types.go
+++ pkg/domain/types.go
@@ -12,6 +12,9 @@ var (
 	LoopEndIsNotFoundError = errors.New("end of the loop is not found while executing one command at a time ")
 )
 
+// Operation is a function which is related to a Command
+// and executed when the related Command is pointed
 type Operation func(b *Brainfuck) error
 
+// Option is supplied by user so that it can customize a brainfuck
 type Option func(b *Brainfuck) error
